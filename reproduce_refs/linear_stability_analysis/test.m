4f601199a364f3c9f8571c2b417b134dc211eee4
duc14102 <ducnv.vnu@gmail.com>
2024-08-12 10:44:37 -0400

    update instruction and matlab code

--- reproduce_refs/linear_stability_analysis/test.m
+++ reproduce_refs/linear_stability_analysis/test.m
@@ -9,6 +9,34 @@ Pe = 1e4; % Peclet number
 Pr = 10.; % Prandtl number
 tau = 0.01; % diffusivity ratio
 
-N = 64;
+kx = 2;
+ky = 0;
 
-[x, D] = fourdif(N, 1);
+N = 8;
+I=eye(N,N);
+O=zeros(N,N);
+
+% Differentiation Matrices 
+[z, D1] = fourdif(N, 1);
+[z2, D2] = fourdif(N, 2);
+
+DX = kx*I;
+DY = ky*I;
+DZ = D1;
+Laplacian = -(kx^2+ky^2)*I + D2;
+
+M = -sin(2*pi*z).*DX + (Pr/Pe)*Laplacian;
+N = -sin(2*pi*z).*DX + (1./Pe)*Laplacian;
+K = -sin(2*pi*z).*DX + (tau/Pe)*Laplacian;
+DU = -2*pi*cos(2*pi*z);
+G = 4.*pi*pi*Ri / (Rp-1.)*I;
+
+A = diag([I I I O I I])
+B = [M, O,DU,-DX, O, O;
+     O, M, O,-DY, O, O;
+     O, O, M,-DZ, G,-G;
+    DX,DY,DZ,  O, O, O;
+     O, O, I,  O, N, O;
+     O, O,Rp,  O, O, K]
+
+[eig_vec, eig_val] = eig(A,B);
