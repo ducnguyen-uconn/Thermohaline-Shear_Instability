98a841d94408ffea1fd6275f4316abd7e0f28534
Duc Nguyen <ducnv.vnu@gmail.com>
2024-10-04 18:25:51 -0400

    fix error regarding 'grad(h_mean(p))'

--- 2-Dedalus3_DirectNumerialSimulation/2.2-Radko2016_FullNSE-Mean_and_Fluctuation/fluc.py
+++ 2-Dedalus3_DirectNumerialSimulation/2.2-Radko2016_FullNSE-Mean_and_Fluctuation/fluc.py
@@ -40,10 +40,9 @@ xbasis = d3.RealFourier(coords['x'], size=Nx, bounds=(0, Lx), dealias=dealias)
 zbasis = d3.RealFourier(coords['z'], size=Nz, bounds=(0, Lz), dealias=dealias)
 
 # define fields
-# um, wm, pm, Tm, Sm: <u>, <w>, <p>, <T>, <S> (<.> is horizontally averaged variable)
-# up, wp, pp, Tp, Sp:  u',  w',  p',  T',  S'
-pm = dist.Field(name='pm', bases=(zbasis)) # horizontally averaged pressure
-pp = dist.Field(name='pp', bases=(xbasis,zbasis)) # pressure fluctuation
+# um, Tm, Sm: <u>, <T>, <S> (<.> is horizontally averaged variable)
+# up, Tp, Sp:  u',  T',  S'
+p = dist.Field(name='p', bases=(xbasis,zbasis)) # pressure 
 um = dist.VectorField(coords,name='um', bases=(zbasis)) # horizontally averaged u(z)
 up = dist.VectorField(coords,name='up', bases=(xbasis,zbasis)) # fluctuation u'(x,z)
 Sm = dist.Field(name='Sm', bases=(zbasis)) # horizontally averaged S(z)
@@ -59,41 +58,43 @@ ex, ez = coords.unit_vector_fields(dist) # get unit vectors in horizontal and ve
 U = um + up # full velocity
 T = Tm + Tp # full temperature
 S = Sm + Sp # full salinity
-# Rho = (S-T)+(1-Rp)*z # density
-# Rhom = (Sm-Tm)+(1-Rp)*z # horizontally averaged density
+umx = um@ex
+umz = um@ez
+upx = up@ex
+upz = up@ez
+
 # create constant sub-field for incompressible flow condition's equation
-tau_pm = dist.Field(name='tau_pm') 
-tau_pp = dist.Field(name='tau_pp') 
+tau_p = dist.Field(name='tau_p') 
 # because this term is only a contant added to the equation, we don't need to instantiate it for bases system
 
 # First-order form: "div(A)" becomes "trace(grad_A)"
 lap = lambda A: d3.div(d3.grad(A)) # First-order form: "lap(f)" becomes "div(grad_f)"
 dx = lambda A: d3.Differentiate(A, coords['x']) 
 dz = lambda A: d3.Differentiate(A, coords['z'])
-vol_avg = lambda A: d3.Average(A) # Horizontal mean of A 
-h_mean = lambda A: d3.Average(A,'x') # Horizontal mean of A
-
+vol_avg = lambda A: d3.Integrate(A)/(Nx*Nz)
+# vol_avg = lambda A: d3.Average(A) # Horizontal mean of A 
+# h_mean = lambda A: d3.Average(A,'x') # Horizontal mean of A <------- this function will get ERROR for computing 'grad(h_mean(p))'
+h_mean = lambda A: d3.Integrate(A,'x')/Nx # <----- highly recommendation for computing 'grad(h_mean(p))' instead
 Ubg['g'] = np.sin(2*pi*z)
 
 # Problem
-problem = d3.IVP([pm,pp,tau_pm, tau_pp,
+problem = d3.IVP([p,tau_p,
                   um,up,
                   Tm,Tp, 
                   Sm,Sp], namespace=locals())
 
-problem.add_equation("trace(grad(um))+tau_pm = 0")
-problem.add_equation("trace(grad(up))+tau_pp = 0")
-problem.add_equation("integ(pm) = 0") # Pressure gauge
-problem.add_equation("integ(pp) = 0") # Pressure gauge
+problem.add_equation("trace(grad(U))+tau_p = 0")
+problem.add_equation("integ(p) = 0") # Pressure gauge
 
-problem.add_equation("dt(um) + grad(pm) - (Pr/Pe)*lap(um) - (4*pi*pi*Ri/(Rp-1))*(Tm-Sm)*ez = - dz(h_mean(up@ez*up))")
-problem.add_equation("dt(up) + Ubg*dx(up) + up@ez*dz(Ubg)*ex + grad(pp) - (Pr/Pe)*lap(up) - (4*pi*pi*Ri/(Rp-1))*(Tp-Sp)*ez = - up@grad(um)-um@grad(up)-up@grad(up)+dz(h_mean(up@ez*up))")
+problem.add_equation("dt(um) + grad(h_mean(p)) - (Pr/Pe)*lap(um) - (4*pi*pi*Ri/(Rp-1))*(Tm-Sm)*ez = - dz(h_mean(upz*up))")
+problem.add_equation("dt(up) + Ubg*dx(up) + upz*dz(Ubg)*ex + grad(p-h_mean(p)) - (Pr/Pe)*lap(up) - (4*pi*pi*Ri/(Rp-1))*(Tp-Sp)*ez = - up@grad(um)-um@grad(up)-up@grad(up)+dz(h_mean(upz*up))")
 
-problem.add_equation("dt(Tm)              - (1./Pe)*dz(dz(Tm))      = - dz(h_mean(up@ez*Tp))")
-problem.add_equation("dt(Tp) + Ubg*dx(Tp) - (1./Pe)*lap(Tp) - up@ez = - up@grad(Tm)-um@grad(Tp)-up@grad(Tp)+dz(h_mean(up@ez*Tp))")
+problem.add_equation("dt(Tm)              - (1./Pe)*dz(dz(Tm)) - umz = - dz(h_mean(upz*Tp))")
+problem.add_equation("dt(Tp) + Ubg*dx(Tp) - (1./Pe)*lap(Tp) - upz = - up@grad(Tm)-um@grad(Tp)-up@grad(Tp)+dz(h_mean(upz*Tp))")
 
-problem.add_equation("dt(Sm)             - (tau/Pe)*dz(dz(Sm))           = - dz(h_mean((up@ez)*Sp))")
-problem.add_equation("dt(Sp) + Ubg*dx(Sp) - (tau/Pe)*lap(Sp) - Rp*(up@ez) = - up@grad(Sm)-um@grad(Sp)-up@grad(Sp)+dz(h_mean((up@ez)*Sp))")
+problem.add_equation("dt(Sm)             - (tau/Pe)*dz(dz(Sm)) - Rp*(umz) = - dz(h_mean((upz)*Sp))")
+problem.add_equation("dt(Sp) + Ubg*dx(Sp) - (tau/Pe)*lap(Sp) - Rp*(upz) = - up@grad(Sm)-um@grad(Sp)-up@grad(Sp)+dz(h_mean((upz)*Sp))")
+
 
 # timestepper = d3.RK443
 timestepper = d3.RK222
@@ -104,13 +105,9 @@ solver.stop_sim_time = stop_sim_time
 
 # Initial conditions
 if not restart:
-    # pm.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
-    pp.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
-    # um.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
+    p.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
     up.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
-    # Tm.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
     Tp.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
-    # Sm.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
     Sp.fill_random('g', seed=42, distribution='normal', scale=1e-4) # Random noise
     file_handler_mode = 'overwrite'
     initial_timestep = 0.02
@@ -124,8 +121,8 @@ else:
 # store instantaneous data for analysis later
 # first, we save horizontal averaged fields (1D array) following small output time-step
 timehistory = solver.evaluator.add_file_handler('timehistory', sim_dt=1.0, max_writes=10000, mode=file_handler_mode)
-timehistory.add_task(um@ex, name='um') # horizontal averaged x-axis velocity
-timehistory.add_task(um@ez, name='wm') # horizontal averaged z-axis velocity, optional, becasue wm=0
+timehistory.add_task(umx, name='um') # horizontal averaged x-axis velocity
+timehistory.add_task(umz, name='wm') # horizontal averaged z-axis velocity, optional, becasue wm=0
 timehistory.add_task(Tm, name='Tm') # horizontal averaged temperature
 timehistory.add_task(Sm, name='Sm') # horizontal averaged sanility
 timehistory.add_task(dz(Sm), name='S_grad') # horizontally averaged profiles of scalar gradients
@@ -136,6 +133,11 @@ timehistory.add_task(h_mean(U@ez*S)/dz(Sm), name='S_tur_diff') # horizontally av
 timehistory.add_task(h_mean(U@ez*T)/dz(Tm), name='T_tur_diff')
 # timehistory.add_task(h_mean(U@ez*Rho)/dz(Rhom), name='den_tur_diff')
 
+timehistory.add_task(vol_avg(np.sqrt(upx*upx)), name='urms')
+timehistory.add_task(vol_avg(np.sqrt(upz*upz)), name='wrms')
+timehistory.add_task(vol_avg(np.sqrt(Tp*Tp)), name='Trms')
+timehistory.add_task(vol_avg(np.sqrt(Sp*Sp)), name='Srms')
+
 # timehistory.add_task(vol_avg((U@ez)*S)/(KS*dT/Lz), name='nusselt_s')
 # timehistory.add_task(vol_avg((U@ez)*S)/(KT*dT/Lz), name='nusselt_t')
 # timehistory.add_task(up@ex*up@ex*Lz/nu, name='Re_x')
@@ -145,17 +147,17 @@ timehistory.add_task(h_mean(U@ez*T)/dz(Tm), name='T_tur_diff')
 # second, we save horizontal averaged fields (1D array) and coressponding pluctuation fields (2D array) following higher output time-step  
 # this can help us to construct full fields for preocessing later
 snapshot = solver.evaluator.add_file_handler('snapshots', sim_dt=50.0, max_writes=10000, mode=file_handler_mode)
-snapshot.add_task(um@ex, name='um')
-snapshot.add_task(um@ez, name='wm') #optional, becasue wm=0
-snapshot.add_task(up@ex, name='up')
-snapshot.add_task(up@ez, name='wp')
+snapshot.add_task(umx, name='um')
+snapshot.add_task(umz, name='wm') #optional, becasue wm=0
+snapshot.add_task(upx, name='up')
+snapshot.add_task(upz, name='wp')
 snapshot.add_task(Tm, name='Tm')
 snapshot.add_task(Tp, name='Tp')
 snapshot.add_task(Sm, name='Sm')
 snapshot.add_task(Sp, name='Sp')
-snapshot.add_task(pm+pp, name='p')
-# snapshot.add_task((S-T)+(1-Rp)*z, name='Rho')
-snapshot.add_task(-d3.div(d3.skew(um+up)), name='vorticity')
+snapshot.add_task(p, name='p')
+# snapshot.add_task((Sm+Sp-Tm-Tp)+(1-Rp)*z, name='Rho')
+snapshot.add_task(-d3.div(d3.skew(U)), name='vorticity')
 # store data to restart later
 # checkpoints = solver.evaluator.add_file_handler('checkpoints', sim_dt=100, max_writes=1, mode=file_handler_mode)
 # checkpoints.add_tasks(solver.state)
